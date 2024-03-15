f9a47cfad7a8ea7814ebf7416fb1381369be8ccc
maroua99 <maroua-guesmia@outlook.fr>
2024-03-16 00:16:45 +0100

    16.03

---
+++ JS.js
@@ -0,0 +1,9 @@
+function redirectToAnotherPage() {
+    // Replace "target.html" with the filename of the HTML page you want to navigate to
+    window.location.href = "Sign_in.html";
+  }
+
+  function redirectToAnotherPage() {
+    // Replace "target.html" with the filename of the HTML page you want to navigate to
+    window.location.href = "Sign_in.html";
+  }
